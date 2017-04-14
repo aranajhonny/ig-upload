2dbac331fda2bc3308b94cafbd11b8581d22bf51
aranajhonny <jhonnyjosearana@gmail.com>
2017-04-14 07:40:45 -0430

    Bump.

--- dist/help.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env node
-"use strict";
-
-console.log("\n    Usage: ig-upload [options] [command]\n\n    Commands:\n      login - login and create session\n      upload - upload images or videos\n      help - display help\n");
-process.exit(0);
